23632f27ed8600fc94edfa14dbdd6aaa0404a561
Rinat Bazhanov <Bazhanov-rinat@yandex.ru>
2023-09-06 08:49:36 +0300

    develop-mesto-full(1.3)

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -100,7 +100,7 @@ export default class Api {
 // })
 
 export const api = new Api({
-    url: 'http://api.Bazhanov.Rinat.nomore.nomoredomainsicu.ru',
+    url: 'https://api.Bazhanov.Rinat.nomore.nomoredomainsicu.ru',
     headers: {
         authorization: `Bearer ${localStorage.getItem('jwt')}`,
         'Content-Type': 'application/json'
